e3325b3de503016c1909ddc1b2c948c2378108f7
Matt Marcum <matt@strangelooper.com>
2015-09-23 23:51:17 -0500

    [master] initial commit, added firebase and ionic

--- config/environment.js
+++ config/environment.js
@@ -4,6 +4,8 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'ember-lift',
     environment: environment,
+    contentSecurityPolicy: { 'connect-src': "'self' https://auth.firebase.com wss://*.firebaseio.com" },
+    firebase: 'https://ember-lift.firebaseio.com//',
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
